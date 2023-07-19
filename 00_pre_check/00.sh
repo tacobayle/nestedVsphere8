a3123749bf3c84230bf15da7062a41e620420562
tacobayle <nicolas.nayle@gmail.com>
2023-07-19 06:31:12 +0200

    20230719_dual_attached

--- 00_pre_check/00.sh
+++ 00_pre_check/00.sh
@@ -279,7 +279,7 @@ if [[ $(jq -c -r .vsphere_underlay.networks.alb $jsonFile) != "null" ]]; then
         test_if_variable_is_defined $(echo $cluster | jq -c .version) "   " "testing if each .vsphere_underlay.networks.alb.$network.k8s_clusters[] have a version defined"
         test_if_variable_is_defined $(echo $cluster | jq -c .cni) "   " "testing if each .vsphere_underlay.networks.alb.$network.k8s_clusters[] have a cni defined"
         if [[ $(echo $cluster | jq -c -r .cni) == "antrea" || $(echo $cluster | jq -c -r .cni) == "calico" ]] ; then
-          echo "   +++ cni is $(echo $cluster | jq -c -r .cni)"
+          echo "   +++ cni is $(echo $cluster | jq -c -r .cni) which is supported"
         else
           echo "   +++ cni $(echo $cluster | jq -c -r .cni) is not supported - cni should be either \"calico\" or \"antrea\""
           exit 255
@@ -287,7 +287,7 @@ if [[ $(jq -c -r .vsphere_underlay.networks.alb $jsonFile) != "null" ]]; then
         test_if_variable_is_defined $(echo $cluster | jq -c .cni_version) "   " "testing if each .vsphere_underlay.networks.alb.$network.k8s_clusters[] have a cni_version defined"
         test_if_variable_is_defined $(echo $cluster | jq -c .ako_version) "   " "testing if each .vsphere_underlay.networks.alb.$network.k8s_clusters[] have a ako_version defined"
         if [[ $(echo $cluster | jq -c -r .ako_version) == "1.10.1" ]] ; then
-          echo "   +++ ako_version is $(echo $cluster | jq -c -r .ako_version)"
+          echo "   +++ ako_version is $(echo $cluster | jq -c -r .ako_version) which is supported"
         else
           echo "   +++ ako_version $(echo $cluster | jq -c -r .ako_version) is not supported"
           exit 255
