b0eda411a062f68e17f07ebe6546402f9266c843
tacobayle <nicolas.nayle@gmail.com>
2023-07-19 08:51:27 +0200

    20230719_dual_attached

--- 10_nsx_alb_config/apply.sh
+++ 10_nsx_alb_config/apply.sh
@@ -2,4 +2,4 @@
 jsonFile="/root/avi.json"
 source /nestedVsphere8/bash/tf_init_apply.sh
 #
-tf_init_apply "Configuration of ALB controller - This should take less than 60 minutes" /nestedVsphere8/10_nsx_alb_config /nestedVsphere8/log/09.stdout /nestedVsphere8/log/09.stderr $jsonFile
+tf_init_apply "Configuration of ALB controller - This should take less than 60 minutes" /nestedVsphere8/10_nsx_alb_config /nestedVsphere8/log/10.stdout /nestedVsphere8/log/10.stderr $jsonFile
