3aa67aaf334feb2f8632f8a27a86003f90930c61
tacobayle <nicolas.nayle@gmail.com>
2023-07-29 13:24:36 +0200

    20230726_dual_attached

--- 09_unmanaged_k8s_clusters/variables.tf
+++ 09_unmanaged_k8s_clusters/variables.tf
@@ -3,5 +3,6 @@ variable "ubuntu_ova_path" {}
 variable "ubuntu_password" {}
 variable "docker_registry_username" {}
 variable "docker_registry_password" {}
+variable "docker_registry_email" {}
 variable "vsphere_nested_password" {}
 variable "avi_password" {}
