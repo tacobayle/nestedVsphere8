d9ee37e93589dd2860e33c8a2bd68cc5b812c460
tacobayle <nicolas.nayle@gmail.com>
2023-08-18 12:41:22 +0200

    20230818_dual_attached

--- bash/run.sh
+++ bash/run.sh
@@ -4,7 +4,7 @@ d=vsphere
 kubectl exec -it pod-${d} -- nestedVsphere8/destroy.sh
 kubectl delete -f cm-${d}.yml ; kubectl delete -f pod-${d}.yml
 ## create
-d=vsphere ; kubectl apply -f cm-${d}.yml ; kubectl apply -f pod-${d}.yml ; sleep 5 ; kubectl exec -it pod-${d} -- nestedVsphere8/apply.sh
+d=vsphere ; kubectl apply -f cm-${d}.yml ; kubectl apply -f pod-${d}.yml ; sleep 5 ; kubectl exec -it pod-${d} -- /bin/bash -c "rm -fr nestedVsphere8 ; git clone https://github.com/tacobayle/nestedVsphere8 -b dual_attached"; kubectl exec -it pod-${d} -- nestedVsphere8/apply.sh
 
 # vsphere-alb
 ## destroy
@@ -12,7 +12,7 @@ d=vsphere-alb
 kubectl exec -it pod-${d} -- nestedVsphere8/destroy.sh
 kubectl delete -f cm-${d}.yml ; kubectl delete -f pod-${d}.yml
 ## create
-d=vsphere-alb ; kubectl apply -f cm-${d}.yml ; kubectl apply -f pod-${d}.yml ; sleep 5 ; kubectl exec -it pod-${d} -- nestedVsphere8/apply.sh
+d=vsphere-alb ; kubectl apply -f cm-${d}.yml ; kubectl apply -f pod-${d}.yml ; sleep 5 ; kubectl exec -it pod-${d} -- /bin/bash -c "rm -fr nestedVsphere8 ; git clone https://github.com/tacobayle/nestedVsphere8 -b dual_attached"; kubectl exec -it pod-${d} -- nestedVsphere8/apply.sh
 
 # vsphere-tanzu-alb-wo-nsx
 ## destroy
@@ -20,7 +20,7 @@ d=vsphere-tanzu-alb-wo-nsx
 kubectl exec -it pod-${d} -- nestedVsphere8/destroy.sh
 kubectl delete -f cm-${d}.yml ; kubectl delete -f pod-${d}.yml
 ## create
-d=vsphere-tanzu-alb-wo-nsx ; kubectl apply -f cm-${d}.yml ; kubectl apply -f pod-${d}.yml ; sleep 5 ; kubectl exec -it pod-${d} -- nestedVsphere8/apply.sh
+d=vsphere-tanzu-alb-wo-nsx ; kubectl apply -f cm-${d}.yml ; kubectl apply -f pod-${d}.yml ; sleep 5 ; kubectl exec -it pod-${d} -- /bin/bash -c "rm -fr nestedVsphere8 ; git clone https://github.com/tacobayle/nestedVsphere8 -b dual_attached"; kubectl exec -it pod-${d} -- nestedVsphere8/apply.sh
 
 # vsphere-nsx
 ## destroy
@@ -28,7 +28,7 @@ d=vsphere-nsx
 kubectl exec -it pod-${d} -- nestedVsphere8/destroy.sh
 kubectl delete -f cm-${d}.yml ; kubectl delete -f pod-${d}.yml
 ## create
-d=vsphere-nsx ; kubectl apply -f cm-${d}.yml ; kubectl apply -f pod-${d}.yml ; sleep 5 ; kubectl exec -it pod-${d} -- nestedVsphere8/apply.sh
+d=vsphere-nsx ; kubectl apply -f cm-${d}.yml ; kubectl apply -f pod-${d}.yml ; sleep 5 ; kubectl exec -it pod-${d} -- /bin/bash -c "rm -fr nestedVsphere8 ; git clone https://github.com/tacobayle/nestedVsphere8 -b dual_attached"; kubectl exec -it pod-${d} -- nestedVsphere8/apply.sh
 
 # vsphere-nsx-alb
 ## destroy
@@ -36,7 +36,7 @@ d=vsphere-nsx-alb
 kubectl exec -it pod-${d} -- nestedVsphere8/destroy.sh
 kubectl delete -f cm-${d}.yml ; kubectl delete -f pod-${d}.yml
 ## create
-d=vsphere-nsx-alb ; kubectl apply -f cm-${d}.yml ; kubectl apply -f pod-${d}.yml ; sleep 5 ; kubectl exec -it pod-${d} -- nestedVsphere8/apply.sh
+d=vsphere-nsx-alb ; kubectl apply -f cm-${d}.yml ; kubectl apply -f pod-${d}.yml ; sleep 5 ; kubectl exec -it pod-${d} -- /bin/bash -c "rm -fr nestedVsphere8 ; git clone https://github.com/tacobayle/nestedVsphere8 -b dual_attached"; kubectl exec -it pod-${d} -- nestedVsphere8/apply.sh
 
 # vsphere-nsx-alb-telco
 ## destroy
@@ -44,7 +44,7 @@ d=vsphere-nsx-alb-telco
 kubectl exec -it pod-${d} -- nestedVsphere8/destroy.sh
 kubectl delete -f cm-${d}.yml ; kubectl delete -f pod-${d}.yml
 ## create
-d=vsphere-nsx-alb-telco ; kubectl apply -f cm-${d}.yml ; kubectl apply -f pod-${d}.yml ; sleep 5 ; kubectl exec -it pod-${d} -- nestedVsphere8/apply.sh
+d=vsphere-nsx-alb-telco ; kubectl apply -f cm-${d}.yml ; kubectl apply -f pod-${d}.yml ; sleep 5 ; kubectl exec -it pod-${d} -- /bin/bash -c "rm -fr nestedVsphere8 ; git clone https://github.com/tacobayle/nestedVsphere8 -b dual_attached"; kubectl exec -it pod-${d} -- nestedVsphere8/apply.sh
 
 # python3 -m http.server
 
