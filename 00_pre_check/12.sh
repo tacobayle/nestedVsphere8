e7dbb7d7e121ef1993417efa73f366b80906f4c2
tacobayle <nicolas.nayle@gmail.com>
2023-08-29 08:38:17 +0200

    20230828_nsx_alb_telco

--- 00_pre_check/12.sh
+++ 00_pre_check/12.sh
@@ -8,11 +8,11 @@ IFS=$'\n'
 #
 echo ""
 echo "==> Downloading TKGm binary file"
-local_file="/root/$(basename $(jq -c -r .tkg.tanzu_bin_location $localJsonFile))"
+local_file="/root/$(basename $(jq -c -r .tkg.tanzu_bin_location $jsonFile))"
 if [ -s "$(echo $local_file)" ]; then echo "   +++ TKG tanzu bin file $local_file is not empty" ; else curl -s -o ${local_file} $(jq -c -r .tkg.tanzu_bin_location $jsonFile) ; fi
 if [ -s "$(echo $local_file)" ]; then echo "   +++ TKG tanzu bin file $local_file is not empty" ; else echo "   +++ TKG tanzu bin $local_file is empty" ; exit 255 ; fi
 
-local_file="/root//$(basename $(jq -c -r .tkg.k8s_bin_location $jsonFile))"
+local_file="/root/$(basename $(jq -c -r .tkg.k8s_bin_location $jsonFile))"
 if [ -s "$(echo $local_file)" ]; then echo "   +++ TKG kubectl file $local_file is not empty" ; else curl -s -o ${local_file} $(jq -c -r .tkg.k8s_bin_location $jsonFile) ; fi
 if [ -s "$(echo $local_file)" ]; then echo "   +++ TKG kubectl file $local_file is not empty" ; else echo "   +++ TKG kubectl file $local_file is empty" ; exit 255 ; fi
 
