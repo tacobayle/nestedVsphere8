a8e0d15cb0891b296768805e4571d57734fbb6c3
tacobayle <nicolas.nayle@gmail.com>
2023-12-01 07:15:54 +0200

    20231201_nsx_tanzu_alb_ns_creation

--- 02_external_gateway/bash/external_gw_routes.sh
+++ 02_external_gateway/bash/external_gw_routes.sh
@@ -4,5 +4,5 @@ external_gw_ip=$(jq -c -r .vsphere_underlay.networks.vsphere.management.external
 #
 #
 #
-scp -o StrictHostKeyChecking=no /root/external_gw_routes.yml ubuntu${external_gw_ip}:/home/ubuntu/routes/external_gw_routes.yml
+scp -o StrictHostKeyChecking=no /root/external_gw_routes.yml ubuntu@${external_gw_ip}:/home/ubuntu/routes/external_gw_routes.yml
 ssh -o StrictHostKeyChecking=no -t ubuntu@${external_gw_ip} "cat /home/ubuntu/routes/external_gw_routes.yml | sudo tee -a /etc/netplan/50-cloud-init.yaml > /dev/null"
