c159b8a8ec3f6147262e3b7924a44b52bbf378df
tacobayle <nicolas.nayle@gmail.com>
2023-12-12 08:14:44 +0100

    20231212_copy_avi_values_to_external

--- 10_nsx_alb_config/ansible_avi_shared.tf
+++ 10_nsx_alb_config/ansible_avi_shared.tf
@@ -4,3 +4,19 @@ resource "null_resource" "retrieve_avi_cert" {
     command = "rm -f /root/avi_cert.txt ; openssl s_client -connect ${var.vsphere_underlay.networks.vsphere.management.avi_nested_ip}:443 2>/dev/null </dev/null |  sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | tee /root/avi_cert.txt > /dev/null "
   }
 }
+
+resource "null_resource" "copy_values_to_external" {
+  depends_on = [null_resource.alb_ansible_config_values]
+  connection {
+    host        = var.vsphere_underlay.networks.vsphere.management.external_gw_ip
+    type        = "ssh"
+    agent       = false
+    user        = "ubuntu"
+    private_key = file("/root/.ssh/id_rsa")
+  }
+
+  provisioner "file" {
+    content = data.template_file.values[0].rendered
+    destination = "/home/ubuntu/avi/values.yml"
+  }
+}
