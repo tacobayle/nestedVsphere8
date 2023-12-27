b83c623d87bdc0d756f082f5d878bfd16c0bc4a8
tacobayle <nicolas.nayle@gmail.com>
2023-12-27 11:31:22 +0100

    20231227_nsx_project_avi_vpc

--- 06_nsx_config/nsx_project.sh
+++ 06_nsx_config/nsx_project.sh
@@ -14,6 +14,7 @@ if $(jq -e '.nsx.config | has("projects")' $jsonFile) ; then
     project_mame=$(echo ${project} | jq -c -r .name)
     tier0_ref=$(echo ${project} | jq -c -r .tier0_ref)
     edge_cluster_ref=$(echo ${project} | jq -c -r .edge_cluster_ref)
+    # retrieve tier0_path
     file_path_json_output="/tmp/nsx_project_tier0_path.json"
     json_key="t0_path"
     /bin/bash /nestedVsphere8/bash/nsx/retrieve_t0_id.sh \
@@ -23,7 +24,7 @@ if $(jq -e '.nsx.config | has("projects")' $jsonFile) ; then
       "${file_path_json_output}" \
       "${json_key}"
     tier0_path=$(jq -c -r ''.${json_key}'' ${file_path_json_output})
-    #
+    # retrieve edge_cluster_path
     file_path_json_output="/tmp/nsx_project_edge_cluster_path.json"
     json_key="edge_cluster_path"
     /bin/bash /nestedVsphere8/bash/nsx/retrieve_edge_cluster_path.sh \
@@ -33,6 +34,7 @@ if $(jq -e '.nsx.config | has("projects")' $jsonFile) ; then
           "${file_path_json_output}" \
           "${json_key}"
     edge_cluster_path=$(jq -c -r ''.${json_key}'' ${file_path_json_output})
+    # create project
     /bin/bash /nestedVsphere8/bash/nsx/create_project.sh "${nsx_manager}" "${nsx_password}" "${project_mame}" "${edge_cluster_path}" "${tier0_path}"
   done
 fi
