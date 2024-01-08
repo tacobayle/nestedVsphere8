414ce944664e7e8ff13abbfacca7195b642695ec
tacobayle <nicolas.nayle@gmail.com>
2024-01-08 15:34:47 +0100

    20240108_clone_vs

--- 10_nsx_alb_config/apply.sh
+++ 10_nsx_alb_config/apply.sh
@@ -2,4 +2,5 @@
 jsonFile="/root/avi.json"
 source /nestedVsphere8/bash/tf_init_apply.sh
 #
+curl https://raw.githubusercontent.com/vmware/alb-sdk/eng/python/avi/sdk/samples/clone_vs.py -o /tmp/clone_vs.py -s
 tf_init_apply "Configuration of ALB controller - This should take less than 60 minutes" /nestedVsphere8/10_nsx_alb_config /nestedVsphere8/log/10.stdout /nestedVsphere8/log/10.stderr $jsonFile
