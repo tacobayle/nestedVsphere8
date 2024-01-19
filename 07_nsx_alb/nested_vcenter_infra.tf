764b95ff245a284c47f51afc30a756e8db06d32b
tacobayle <nicolas.nayle@gmail.com>
2024-01-20 00:26:42 +0100

    20240120_multi_datastore_until_09

--- 07_nsx_alb/nested_vcenter_infra.tf
+++ 07_nsx_alb/nested_vcenter_infra.tf
@@ -2,18 +2,13 @@ data "vsphere_datacenter" "dc_nested" {
   name = var.vsphere_nested.datacenter
 }
 
-data "vsphere_compute_cluster" "compute_cluster_nested" {
-  name          = var.avi.cluster_ref
-  datacenter_id = data.vsphere_datacenter.dc_nested.id
-}
-
 data "vsphere_datastore" "datastore_nested" {
   name = var.avi.datastore_ref
   datacenter_id = data.vsphere_datacenter.dc_nested.id
 }
 
 data "vsphere_resource_pool" "resource_pool_nested" {
-  name          = "${var.avi.cluster}/Resources"
+  name          = "${var.avi.cluster_ref}/Resources"
   datacenter_id = data.vsphere_datacenter.dc_nested.id
 }
 
