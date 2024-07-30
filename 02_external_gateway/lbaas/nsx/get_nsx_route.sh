e49578f9d97717917369d3ee077a761b887d348a
tacobayle <nicolas.nayle@gmail.com>
2024-07-30 15:44:36 +0200

    20240730_lbaas

---
+++ 02_external_gateway/lbaas/nsx/get_nsx_route.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#
+source /home/ubuntu/lbaas/nsx/nsx_api.sh
+source /home/ubuntu/lbaas/avi/alb_api.sh
+#
+results_json="{}"
+output_json_file="${2}"
+IFS=$'\n'
+date_index=$(date '+%Y%m%d%H%M%S')
+jsonFile="/tmp/$(basename "$0" | cut -f1 -d'.')_${date_index}.json"
+jsonFile1="${1}"
+if [ -s "${jsonFile1}" ]; then
+  jq . $jsonFile1 > /dev/null
+else
+  echo "ERROR: jsonFile1 file is not present"
+  exit 255
+fi
+#
+jsonFile2="/home/ubuntu/lbaas/lbaas.json"
+if [ -s "${jsonFile2}" ]; then
+  jq . $jsonFile2 > /dev/null
+else
+  echo "ERROR: jsonFile2 file is not present"
+  exit 255
+fi
+#
+jq -s '.[0] * .[1]' ${jsonFile1} ${jsonFile2} | tee ${jsonFile}
+#
+if $(jq -e '. | has("vs_name")' $jsonFile) ; then
+  vs_name=$(jq -c -r .vs_name $jsonFile)
+else
+  "ERROR: vs_name should be defined"
+  exit 255
+fi
+#
+cookies_file="/tmp/$(basename "$0" | cut -f1 -d'.')_${date_index}_cookies.txt"
+headers_file="/tmp/$(basename "$0" | cut -f1 -d'.')_${date_index}_headers.txt"
+rm -f $cookies_file $headers_file
+#
+/bin/bash /home/ubuntu/lbaas/nsx/create_nsx_api_session.sh $(jq -c -r .nsx_username $jsonFile) $(jq -c -r .nsx_password $jsonFile) $(jq -c -r .nsx_nested_ip $jsonFile) $cookies_file $headers_file
+#
+avi_cookie_file="/tmp/avi_$(basename $0 | cut -d"." -f1)_${date_index}_cookie.txt"
+curl_login=$(curl -s -k -X POST -H "Content-Type: application/json" \
+                                -d "{\"username\": \"$(jq -c -r .avi_username $jsonFile)\", \"password\": \"$(jq -c -r .avi_password $jsonFile)\"}" \
+                                -c ${avi_cookie_file} https://$(jq -c -r .avi_nested_ip $jsonFile)/login)
+csrftoken=$(cat ${avi_cookie_file} | grep csrftoken | awk '{print $7}')
+#
+while true
+do
+  alb_api 3 5 "GET" "${avi_cookie_file}" "${csrftoken}" "$(jq -c -r .avi_tenant $jsonFile)" "$(jq -c -r .avi_version $jsonFile)" "" "$(jq -c -r .avi_nested_ip $jsonFile)" "api/virtualservice?page_size=-1"
+  if [[ $(echo $response_body | jq -c -r '.results | length') -gt 0 && $(echo $response_body | jq -c -r --arg arg "${vs_name}" '[.results[] | select(.name == $arg).name] | length') -eq 1 ]]; then
+    vsvip_ref=$(echo $response_body | jq -c -r --arg arg "${vs_name}" '.results[] | select(.name == $arg).vsvip_ref')
+    alb_api 3 5 "GET" "${avi_cookie_file}" "${csrftoken}" "$(jq -c -r .avi_tenant $jsonFile)" "$(jq -c -r .avi_version $jsonFile)" "" "$(jq -c -r .avi_nested_ip $jsonFile)" "api/vsvip/$(basename ${vsvip_ref})"
+    ip_vip=$(echo ${response_body} | jq -c -r .vip[0].ip_address.addr)
+    tier1_name=$(echo ${response_body} | jq -c -r .tier1_lr)
+    if [[ -z "${ip_vip}" || -z "${tier1_name}" ]]; then
+      echo "retrying..."
+    else
+      break
+    fi
+  else
+    echo "retrying..."
+  fi
+done
+#
+
+#
+#nsx_api 2 2 "GET" $cookies_file $headers_file "" $(jq -c -r .nsx_nested_ip $jsonFile) "policy/api/v1/infra/tier-1s"
+#tier1_id=$(echo $response_body | jq -c -r --arg arg "${tier1_name}" '.results[] | select(.display_name == $arg) | .id')
+#
+while true
+do
+  nsx_api 2 2 "GET" $cookies_file $headers_file "${json_data}" $(jq -c -r .nsx_nested_ip $jsonFile) "policy/api/v1/infra/tier-1s/$(basename ${tier1_name})/static-routes"
+  next_hops=$(echo $response_body | jq -c -r --arg arg "${ip_vip}/32" '[.results[] | select(.network == $arg) | .next_hops[].ip_address]')
+  if [ -z "${next_hops}" ]; then
+    echo "retrying..."
+  else
+    results_json=$(echo $results_json | jq '. += {"date": "'$(date)'", "vs_name": "'${vs_name}'", "vsvip": "'${ip_vip}'", "next_hops": '${next_hops}'}')
+    echo $results_json | tee ${output_json_file} | jq .
+    break
+  fi
+  sleep 10
+done
+#
+rm -f ${jsonFile}
+rm -f ${jsonFile1}
+rm -f $cookies_file $headers_file ${avi_cookie_file}
--- 02_external_gateway/lbaas/python/lbaas.py
+++ 02_external_gateway/lbaas/python/lbaas.py
@@ -14,6 +14,7 @@ from flask_cors import CORS
 # curl -X GET http://127.0.0.1:5000/api/getvipsegment -d '{"vs_name":"signed-pub"}' -H "Content-Type: application/json"
 # curl -X GET http://127.0.0.1:5000/api/getseip -d '{"vs_name":"signed-pub"}' -H "Content-Type: application/json"
 # curl -X GET http://127.0.0.1:5000/api/getsehost -d '{"vs_name":"signed-pub"}' -H "Content-Type: application/json"
+# curl -X GET http://127.0.0.1:5000/api/getnsxroute -d '{"vs_name":"signed-pub"}' -H "Content-Type: application/json"
 
 
 
@@ -212,6 +213,23 @@ def getsehost():
         json.dump(a_dict, outfile)
     folder="/home/ubuntu/lbaas/avi"
     subprocess.run(['/bin/bash', 'get_se_host.sh', json_file, json_output_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=folder)
+    with open(json_output_file, 'r') as results_json:
+        results = json.load(results_json)
+    return results, 201
+
+@app.route('/api/getnsxroute', methods=['GET'])
+def getnsxroute():
+    args_parser_get= reqparse.RequestParser()
+    args_parser_get.add_argument("vs_name", type=str, help="VS Name", required=True)
+    args_parser_get = args_parser_get.parse_args()
+    a_dict = {}
+    a_dict['vs_name'] = args_parser_get['vs_name']
+    json_file='/tmp/getnsxroute_' + a_dict['vs_name'] + '.json'
+    json_output_file='/tmp/getnsxroute_output_' + a_dict['vs_name'] + '.json'
+    with open(json_file, 'w') as outfile:
+        json.dump(a_dict, outfile)
+    folder="/home/ubuntu/lbaas/nsx"
+    subprocess.run(['/bin/bash', 'get_nsx_route.sh', json_file, json_output_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=folder)
     with open(json_output_file, 'r') as results_json:
         results = json.load(results_json)
     return results, 201
