ce68c6b2e402831299a950988442f6bd0f94dc3f
tacobayle <nicolas.nayle@gmail.com>
2024-08-19 14:18:15 +0200

    20240819_demo-http-apps

--- 02_external_gateway/bash/yaml_replace_avi_domain.sh
+++ 02_external_gateway/bash/yaml_replace_avi_domain.sh
@@ -8,10 +8,33 @@ domain="$(jq -c -r '.avi_domain_prefix' $jsonFile).$(jq -c -r '.external_gw.bind
 # ingress - update host
 ingress=$(yq . /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-ingress.yml)
 ingress=$(echo $ingress | jq '. | del (.spec.rules[0].host)')
-ingress=$(echo $ingress | jq '.spec.rules[0] += {"host": "ingress.'${domain}'"}')
+ingress=$(echo $ingress | jq '.spec.rules[0] += {"host": "v1.'${domain}'"}')
+ingress=$(echo $ingress | jq '. | del (.spec.rules[1].host)')
+ingress=$(echo $ingress | jq '.spec.rules[1] += {"host": "v2.'${domain}'"}')
+ingress=$(echo $ingress | jq '. | del (.spec.rules[2].host)')
+ingress=$(echo $ingress | jq '.spec.rules[2] += {"host": "v3.'${domain}'"}')
 echo $ingress | yq -y . | tee /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-ingress.yml > /dev/null
 # crd - update fqdn
-crd=$(yq . /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crds.yml)
+crd=$(yq . /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crd-1.yml)
 crd=$(echo $crd | jq '. | del (.spec.virtualhost.fqdn)')
-crd=$(echo $crd | jq '.spec.virtualhost += {"fqdn": "ingress.'${domain}'"}')
-echo $crd | yq -y . | tee /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crds.yml > /dev/null
+crd=$(echo $crd | jq '.spec.virtualhost += {"fqdn": "v1.'${domain}'"}')
+echo $crd | yq -y . | tee /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crd-1.yml > /dev/null
+#
+crd=$(yq . /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crd-2.yml)
+crd=$(echo $crd | jq '. | del (.spec.virtualhost.fqdn)')
+crd=$(echo $crd | jq '.spec.virtualhost += {"fqdn": "v2.'${domain}'"}')
+echo $crd | yq -y . | tee /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crd-2.yml > /dev/null
+#
+crd=$(yq . /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crd-3.yml)
+crd=$(echo $crd | jq '. | del (.spec.virtualhost.fqdn)')
+crd=$(echo $crd | jq '.spec.virtualhost += {"fqdn": "v3.'${domain}'"}')
+echo $crd | yq -y . | tee /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crd-3.yml > /dev/null
+#
+cat /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crd-1.yml | tee /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crds.yml
+echo "---" tee -a /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crds.yml
+cat /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crd-2.yml | tee -a /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crds.yml
+echo "---" tee -a /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crds.yml
+cat /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crd-3.yml | tee -a /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crds.yml
+rm /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crd-1.yml > /dev/null
+rm /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crd-2.yml > /dev/null
+rm /home/ubuntu/$(jq -c -r .yaml_directory $jsonFile)/demo-http-crd-3.yml > /dev/null
