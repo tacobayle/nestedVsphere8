aae71cf3253e87dabc33755a284f6ed2aea6d4a9
tacobayle <nicolas.nayle@gmail.com>
2024-09-01 06:53:40 +0200

    20240901_apply_state

--- apply.sh
+++ apply.sh
@@ -12,14 +12,18 @@ fi
 #
 # 00_pre_check
 #
-pre_check_scripts='["00.sh", "02.sh", "03.sh", "04.sh", "05.sh", "07.sh", "08.sh", "10.sh", "12.sh", "13.sh"]'
-for script in $(echo ${pre_check_scripts} | jq -c -r .[])
-do
-  /bin/bash /nestedVsphere8/00_pre_check/${script}
-  if [ $? -ne 0 ] ; then exit 1 ; fi
-  touch "/root/${script}"
-done
+if [ -f "/root/00_pre_check" ]; then
+  echo "Skipping 00_pre_check"
+else
+  pre_check_scripts='["00.sh", "02.sh", "03.sh", "04.sh", "05.sh", "07.sh", "08.sh", "10.sh", "12.sh", "13.sh"]'
+  for script in $(echo ${pre_check_scripts} | jq -c -r .[])
+  do
+    /bin/bash /nestedVsphere8/00_pre_check/${script}
+    if [ $? -ne 0 ] ; then exit 1 ; fi
+  done
+fi
 #
+touch "/root/00_pre_check"
 if [ -z "${slack_webhook_url}" ] ; then echo "ignoring slack update" ; else curl -X POST -H 'Content-type: application/json' --data '{"text":"'$(date "+%Y-%m-%d,%H:%M:%S")', '${deployment}': 00_pre_check done"}' ${slack_webhook_url} >/dev/null 2>&1 ; fi
 #
 jsonFile="/root/variables.json"
@@ -32,7 +36,6 @@ echo ""
 # Environment Creation
 #
 output_file="/root/output.txt"
-rm -f ${output_file}
 #
 apply_scripts='["01_underlay_vsphere_directory", "02_external_gateway", "03_nested_vsphere", "04_networks", "05_nsx_manager", "06_nsx_config", "07_nsx_alb", "08_app", "09_lbaas", "10_unmanaged_k8s_clusters", "11_nsx_alb_config", "12_vsphere_with_tanzu", "13_tkgm"]'
 for folder in $(echo ${apply_scripts} | jq -c -r .[])
